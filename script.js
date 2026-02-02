c21c070e6f77e0123c1493251b481ade6324ca52
kartik bhosale <kartikbirangal4@gmail.com>
2026-02-02 16:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 function goNext(){
   document.getElementById("page1").classList.add("hidden");
   document.getElementById("page2").classList.remove("hidden");
+  document.getElementById("bgm").play();
 }
+
+let text = "You make my world brighter every day 💖 I’m so lucky to have you.";
+let i = 0;
 
 function showLetter(){
   document.getElementById("letter").classList.remove("hidden");
   document.getElementById("photos").classList.add("hidden");
+
+  i = 0;
+  document.getElementById("loveText").innerHTML = "";
+
+  let typing = setInterval(() => {
+    document.getElementById("loveText").innerHTML += text[i];
+    i++;
+    if(i >= text.length) clearInterval(typing);
+  }, 50);
 }
 
 function showPhotos(){
